1ceb700b64182cdcf53beb5b7d53a9463caee671
JackWanaCode <kazucuong@yahoo.com>
2018-06-21 23:36:44 +0000

    correct 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i < '8'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
