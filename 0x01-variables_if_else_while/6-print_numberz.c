25d442dfe975ebcfd34ea8578b6d4c42be090b18
JackWanaCode <kazucuong@yahoo.com>
2018-06-21 22:19:30 +0000

    correct 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,11 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i;
 	int arr[11] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-	while (i < 11)
-	{
+	for (i = 0; i < 11; i++)
 		putchar(arr[i]);
-		i++;
-	}
 	putchar('\n');
 	return (0);
 }
