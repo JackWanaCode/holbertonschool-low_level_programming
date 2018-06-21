cc9259a0f9e4c9647ac5cd8ae81631aa6c5251bc
JackWanaCode <kazucuong@yahoo.com>
2018-06-21 19:31:34 +0000

    correcting 0 and 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * maint - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -16,8 +16,8 @@ int main(void)
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
-		printf("%i is negativ\n", n);
+		printf("%i is negative\n", n);
 	else
-		printf("%i is zero", n);
+		printf("%i is zero\n", n);
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * maint - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
