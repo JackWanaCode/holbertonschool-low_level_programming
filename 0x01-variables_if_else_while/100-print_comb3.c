bdbe7ff5076b8709e98852ceafa61115928f9378
JackWanaCode <kazucuong@yahoo.com>
2018-06-22 00:20:17 +0000

    first commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+	int count = 1;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (count < 100)
+			{
+				putchar(',');
+				putchar(' ');
+				count++;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
