ed1b33ed64118f6a3294c748945e99de845cb2fb
JackWanaCode <kazucuong@yahoo.com>
2018-06-22 20:16:37 +0000

    correct 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	fprintf(stdout, "and that piece of art is useful\" - Dora\
- Korpar, 2015-10-19\n");
+	write(0, "and that piece of art is useful\" - Dora\
+ Korpar, 2015-10-19\n", 80);
 	return (1);
 }
