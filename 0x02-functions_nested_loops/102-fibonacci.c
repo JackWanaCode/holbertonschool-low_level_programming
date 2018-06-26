a2868ba1f3c7a9bea58728be165f37fd2d61b3b6
JackWanaCode <kazucuong@yahoo.com>
2018-06-26 22:35:52 +0000

    correct 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	float first = 1;
-	float second = 1;
-	float fibnum = 2;
+	long int first = 1;
+	long int second = 1;
+	long int fibnum = 2;
 	int count = 0;
 
 	putchar(second + '0');
-	while (count < 47)
+	while (count < 49)
 	{
-		printf(", %.0f", fibnum);
+		printf(", %li", fibnum);
 		first = second;
 		second = fibnum;
 		fibnum = first + second;
