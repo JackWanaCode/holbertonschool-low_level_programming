4ca3aba94379334425fd6c6351344fc530d5895f
JackWanaCode <kazucuong@yahoo.com>
2018-06-26 22:07:50 +0000

    correct fib nums

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,14 +11,16 @@ int main(void)
 	int first = 1;
 	int second = 1;
 	int fibnum = 2;
+	int count = 0;
 
 	putchar(second + '0');
-	while (fibnum < 50)
+	while (count < 49)
 	{
 		printf(", %i", fibnum);
 		first = second;
 		second = fibnum;
 		fibnum = first + second;
+		count++;
 	}
 	putchar('\n');
 	return (0);
