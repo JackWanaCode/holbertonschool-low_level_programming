fd24742b328cefaa19cb31c952b9311404f6e047
JackWanaCode <kazucuong@yahoo.com>
2018-06-26 17:59:47 +0000

    correct 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,18 +13,21 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			if (j != 0)
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (k < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
-			if (k < 10 && j != '0')
-			{
 				_putchar(' ');
 				_putchar(k + '0');
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
