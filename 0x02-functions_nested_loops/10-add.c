2d9a814a38e09605c0e29f16d98e0bce28f9ed84
JackWanaCode <kazucuong@yahoo.com>
2018-06-26 20:21:03 +0000

    correct 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,11 @@
 
 /**
  * add - take a sum of 2 numbers
+ * @n1: first number
+ * @n2: second number
  * Return: nothing
  */
 int add(int n1, int n2)
 {
-       return (n1+n2);
+	return (n1 + n2);
 }
