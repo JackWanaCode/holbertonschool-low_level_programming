bd207714d8639ed5a76ec733e6358e45750fd823
JackWanaCode <kazucuong@yahoo.com>
2018-06-28 23:24:42 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_number - Entry point
+ * Description: putchar any kind of integer
+ * @n: number to be print
+ * Return: 0 Always
+ */
+void print_number(int n)
+{
+	int temp1 = n / 10;
+	int temp2 = 10;
+	int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	num = n;
+	while (temp1 / 10)
+	{
+		temp1 /= 10;
+		temp2 *= 10;
+	}
+	while (temp2 > 1 && num > 9)
+	{
+		_putchar(n / temp2 + '0');
+		n = n % temp2;
+		temp2 /= 10;
+	}
+	_putchar(n % 10 + '0');
+}
