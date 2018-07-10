d81707672dd445648f3b116e90431e972c6e3f01
JackWanaCode <kazucuong@yahoo.com>
2018-07-10 22:35:19 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strspn - Entry point
+ * Description: return max index + 1 that stored a given string.
+ * @s: bigger string.
+ * @accept: given string
+ *
+ * Return: max index + 1
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int max = 0;
+
+	for (i = 0; *(accept + i) != '\0'; i++)
+	{
+		for (j = 0; *(s + j) != '\0'; j++)
+		{
+			if (*(s + j) == *(accept + i))
+			{
+				if (max < j)
+					max = j;
+				break;
+			}
+		}
+		if (*(s + j) == '\0')
+			return (j);
+	}
+	return (max + 1);
+}
