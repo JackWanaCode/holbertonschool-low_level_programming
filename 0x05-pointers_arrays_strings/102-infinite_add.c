92ab9c4ec78e6c37f3b20b64beb413d4d0847125
JackWanaCode <kazucuong@yahoo.com>
2018-07-10 01:31:18 +0000

    correct 102

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,22 +1,6 @@
 #include "holberton.h"
 
 /**
- * add2char - Entry point
- * Description: plus 2 number character
- * @c1: first num char
- * @c2: second num char
- * @ret: return value in single num character
- * @m: value to remember after plus
- *
- * Return: nothing
- */
-void add2char(char *c1, char *c2, int *ret, int *m)
-{
-	*ret = *c1 - '0' + *c2 - '0';
-	*m = *ret / 10;
-	*ret = *ret % 10;
-}
-/**
  * infinite_add - Entry point
  * Description: plus 2 number strings
  * @n1: first string
@@ -31,7 +15,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i, cut;
 	int l_of_n1, l_of_n2;
 	int m1 = 0, m2 = 0, overFlow = 0, ret = 0;
-	char zero = '0';
 	char ret_char[size_r];
 
 	for (l_of_n1 = 0; *(n1 + l_of_n1) != '\0'; l_of_n1++)
@@ -46,13 +29,21 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		ret = 0;
 		overFlow = 0;
 		if (l_of_n1 >= 0 && l_of_n2 >= 0)
-			add2char((n1 + l_of_n1), (n2 + l_of_n2), &ret, &m2);
+		{
+			ret = *(n1 + l_of_n1) - '0' + *(n2 + l_of_n2) - '0';
+			m2 = ret / 10;
+			ret = ret % 10;
+		}
 		else if (l_of_n1 < 0 && l_of_n2 >= 0)
-			add2char(&zero, (n2 + l_of_n2), &ret, &m2);
+		{
+			ret = *(n2 + l_of_n2) - '0';
+			ret = ret % 10;
+		}
 		else if (l_of_n1 >= 0 && l_of_n2 < 0)
-			add2char((n1 + l_of_n1), &zero, &ret, &m2);
-		else if (l_of_n1 < 0 && l_of_n2 < 0)
-			add2char(&zero, &zero, &ret, &m2);
+		{
+			ret = *(n1 + l_of_n1) - '0';
+			ret = ret % 10;
+		}
 		ret = ret + m1;
 		if (ret > 9)
 		{
@@ -73,7 +64,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		l_of_n1--;
 		l_of_n2--;
 	}
-	for (i = 0; i < size_r - 1; i++)
+	for (i = 0; i + cut < size_r; i++)
 		*(r + i) = *(ret_char + i + cut);
 	*(r + i) = '\0';
 	return (r);
