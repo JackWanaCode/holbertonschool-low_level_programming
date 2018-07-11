b6046dba85b0140df8833ed5707d6299d37c6df6
JackWanaCode <kazucuong@yahoo.com>
2018-07-11 06:19:18 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_diagsums - Entry point
+ * Description: sum of the two diagonals of a square matrix of integers.
+ * @a: a pointer point to an 2d array
+ * @size: size of 2d array
+ * Return: no thing.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int ret1 = 0;
+	int ret2 = 0;
+
+	for (i = 0; i < (size * size); i = i + size + 1)
+	{
+		ret1 += *(a + i);
+	}
+	for (i = size - 1; i < (size * size - (size - 1)); i = i + size -1)
+	{
+		ret2 += *(a + i);
+	}
+	printf("%i, %i\n", ret1, ret2);
+}
