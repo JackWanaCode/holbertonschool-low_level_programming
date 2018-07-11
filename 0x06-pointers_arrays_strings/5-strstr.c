72aa458b2d6be63c374f074b20f97b921bc8fcf2
JackWanaCode <kazucuong@yahoo.com>
2018-07-11 00:52:58 +0000

    first commit

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -13,6 +13,8 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	if (*needle == '\0')
+		return (haystack);
 	for (i = 0; *(needle + i) != '\0'; i++)
 	{
 		for (j = 0; *(haystack + j) != '\0'; j++)
