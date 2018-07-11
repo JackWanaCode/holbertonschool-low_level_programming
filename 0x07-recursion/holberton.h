1856a7a208bf347c005e47e36efb51865a659400
JackWanaCode <kazucuong@yahoo.com>
2018-07-11 15:46:05 +0000

    add prototype

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -48,5 +48,14 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+
 
 #endif
