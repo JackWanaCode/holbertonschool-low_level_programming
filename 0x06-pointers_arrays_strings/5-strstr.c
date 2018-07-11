f681a87bf020cece2dc5b6c5223441d8f7b820d7
JackWanaCode <kazucuong@yahoo.com>
2018-07-11 00:35:25 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strstr - Entry point
+ * Description: find substring in the other string.
+ * @haystack: Other string
+ * @needle: substring will be checked
+ *
+ * Return: return 1st occurence in string if match otherwise return NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; *(needle + i) != '\0'; i++)
+	{
+		for (j = 0; *(haystack + j) != '\0'; j++)
+		{
+			if (*(needle + i) == *(haystack + j))
+			{
+				if (*(needle + i + 1) == '\0')
+					return (haystack + j - i);
+				else if (needle[i + 1] != haystack[j + 1])
+					return (NULL);
+				i++;
+			}
+		}
+	}
+	return (NULL);
+}
