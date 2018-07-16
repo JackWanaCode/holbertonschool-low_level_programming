fc72647be13160466186a88f4cdda22cdde3d6c7
JackWanaCode <kazucuong@yahoo.com>
2018-07-16 23:35:01 +0000

    correct 100

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -11,35 +11,24 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2 && *s1 != '\0')
-		return (wildcmp((s1 + 1), (s2 + 1)));
-	else if (*s2 == '*')
+	if (*s1 == '\0' && *s2 == '\0')
 	{
-		if (*(s1) == *(s2 + 1))
-		{
-			return (wildcmp((s1), (s2 + 1)));
-		}
-		else if (*(s1 + 1) == *(s2 + 1))
-			return (wildcmp((s1 + 1), (s2 + 1)));
-		else if (*s1 == '\0')
-		{
-			if (*(s2 + 1) == '\0')
-				return (1);
-			else if (*(s2 + 1) == '*')
-				return (wildcmp(s1, (s2 + 1)));
-			else if (*(s1 - 1) == *(s2 + 1))
-				return (1);
-			else
-				return (wildcmp(s1, (s2 + 1)));
-		}
-		else
-			return (wildcmp((s1 + 1), s2));
+		return (1);
 	}
-	else if (*s1 != *s2)
-		return (0);
-	else if (*s1 == '\0' && *s2 != '\0')
-		return (0);
-	else if (*s2 == '\0' && *s1 != '\0')
-		return (0);
-	return (1);
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) > 0)
+		{
+			return (1);
+		}
+		if (wildcmp(s1 + 1, s2 ) > 0)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
