2d3cd203a6a454d87f0633fa92324175f0911337
JackWanaCode <kazucuong@yahoo.com>
2018-07-17 16:07:02 +0000

    first commit

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *array = malloc(sizeof(char) * size);
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 	{
 		return (NULL);
 	}
