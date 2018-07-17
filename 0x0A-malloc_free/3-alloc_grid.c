eece227c3945c2316d8ea87554048f11c0950862
JackWanaCode <kazucuong@yahoo.com>
2018-07-17 23:04:25 +0000

    first commit

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -17,14 +17,12 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	twoD_arr = malloc(sizeof(int *) * height);
-	if (twoD_arr == NULL)
-		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		twoD_arr[i] = malloc(sizeof(int) * width);
-		if (twoD_arr[i] == NULL)
-			return (NULL);
 	}
+	if (twoD_arr == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
