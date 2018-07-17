e7e9428cfe3f632fac70fa8fc93bc131dc5ad3dc
JackWanaCode <kazucuong@yahoo.com>
2018-07-17 22:57:13 +0000

    first commit

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - Entry point
+ * Description: making a 2 dimension array
+ * @width: number of column.
+ * @height: number of row.
+ * Return: a pointer to pointer to  new 2d array.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **twoD_arr = NULL;
+	int i, j;
+
+	twoD_arr = malloc(sizeof(int *) * height);
+	if (twoD_arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		twoD_arr[i] = malloc(sizeof(int) * width);
+		if (twoD_arr[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			twoD_arr[i][j] = 0;
+		}
+	}
+	return (twoD_arr);
+}
