82ededbd79bd56c7bbc0c104819861150ffbfda7
JackWanaCode <kazucuong@yahoo.com>
2018-07-19 17:27:32 +0000

    first commit

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - Entry Point
+ * @nmemb: number of element(s) in array
+ * @size: size of each element.
+ *
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *c = NULL;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	c = malloc(nmemb * size);
+	if (c == NULL)
+		return (NULL);
+	while (i < nmemb)
+		c[i++] = 0;
+	return (c);
+}
