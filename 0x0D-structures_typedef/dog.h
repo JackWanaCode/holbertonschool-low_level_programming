301dee4116f6db233f10a7cd9c0fdf833fca5863
JackWanaCode <kazucuong@yahoo.com>
2018-07-23 20:04:04 +0000

    add comment to dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_
 #define _DOG_
 
+/**
+ * struct dog - Store data of name, age and owner of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: No
+ */
 struct dog
 {
 	char *name;
