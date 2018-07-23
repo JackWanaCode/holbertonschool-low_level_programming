f3125c9f93058653fbc68ab066d37b1da18e0c45
JackWanaCode <kazucuong@yahoo.com>
2018-07-23 17:51:15 +0000

    first commit

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - Entry Point
+ * Description: print the file name
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
