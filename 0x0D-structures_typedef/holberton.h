18dcd97b0e299661de6af93eb97fbd7ae1ed3fa9
JackWanaCode <kazucuong@yahoo.com>
2018-07-23 20:28:45 +0000

    first commit

--- 0x0D-structures_typedef/holberton.h
+++ 0x0D-structures_typedef/holberton.h
@@ -80,6 +80,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 
 
