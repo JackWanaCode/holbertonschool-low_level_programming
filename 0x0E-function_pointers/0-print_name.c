d815a60e9ab9468da23343d92f83e1e4dff40900
JackWanaCode <kazucuong@yahoo.com>
2018-07-25 17:37:42 +0000

    first commit

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Entry point
+ * Description: print the name as format requested
+ * @name: string to be printed
+ * @f: pointer to function
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
