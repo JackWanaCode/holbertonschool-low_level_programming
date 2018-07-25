79b5c440894c0c469abf26ee29137cfbd84d0f2a
JackWanaCode <kazucuong@yahoo.com>
2018-07-25 18:09:22 +0000

    first commit

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Entry point
+ * Description: print element in an array
+ * @array: array of integer
+ * @size: size of array
+ * @action: function pointer
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
