680d9fd1d2ab705fc03ebc412f6a34752087089e
JackWanaCode <kazucuong@yahoo.com>
2018-07-26 00:14:10 +0000

    correct 2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -15,10 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
-	{
-		if ((*cmp)(array[i]))
-			return (i);
-	}
+	if (array && cmp)
+		for (i = 0; i < size; i++)
+		{
+			if ((*cmp)(array[i]))
+				return (i);
+		}
 	return (-1);
 }
