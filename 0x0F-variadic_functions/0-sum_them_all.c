61220e6f786ebe6458dc17e570cfe3dd6db1be39
JackWanaCode <kazucuong@yahoo.com>
2018-07-27 15:44:21 +0000

    first commit

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Entry point
+ * Description: sum of all its parameters
+ * @n: number of argument
+ * @...: elipses
+ *
+ * Return: Sum of parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list arguments;
+
+	if (n == 0)
+		return (0);
+	va_start(arguments, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(arguments, int);
+	va_end(arguments);
+	return (sum);
+}
---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef _VARARIADIC_FUNC_
+#define _VARARIADIC_FUNC_
+int sum_them_all(const unsigned int n, ...);
+
+#endif
