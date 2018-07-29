46edac62a1c00be0ecd4370cea2eb45f09fba440
JackWanaCode <kazucuong@yahoo.com>
2018-07-29 17:10:28 +0000

    add separator is NULL case

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -17,15 +17,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *print_str;
 	va_list arguments;
 
-	if (!separator)
-		return;
 	va_start(arguments, n);
 	for (i = 0; i < n; i++)
 	{
 		print_str = va_arg(arguments, char *);
 		if (!print_str)
 			print_str = "(nil)";
-		if (i > 0)
+		if (i > 0 && separator)
 			printf("%s%s", separator, print_str);
 		else
 			printf("%s", print_str);
