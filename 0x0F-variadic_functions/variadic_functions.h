098d8656b814fd788440607493df114be07d66a1
JackWanaCode <kazucuong@yahoo.com>
2018-07-29 04:23:39 +0000

    update prototype

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _VARARIADIC_FUNC_
 #define _VARARIADIC_FUNC_
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
