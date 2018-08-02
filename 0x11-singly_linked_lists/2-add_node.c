d917ec64d7bc8e400c4fed6f179cce26cd949ca1
JackWanaCode <kazucuong@yahoo.com>
2018-08-02 22:38:45 +0000

    return the old version of 2-add_node

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -14,27 +14,17 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	char *_strdup;
-	int i = 0;
+	int i;
 
+	_strdup = strdup(str);
+	for (i = 0; _strdup[i] != '\0'; i++)
+		;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	if (!str)
-	{
-		new->str = NULL;
-		new->len = i;
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		_strdup = strdup(str);
-		for (i = 0; _strdup[i] != '\0'; i++)
-			;
-		new->str = _strdup;
-		new->len = i;
-		new->next = *head;
-		*head = new;
-	}
+	new->str = _strdup;
+	new->len = i;
+	new->next = *head;
+	*head = new;
 	return (new);
 }
