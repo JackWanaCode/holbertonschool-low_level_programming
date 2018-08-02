899e474fbc815957c6a5e6a1fd44c80df87b931e
JackWanaCode <kazucuong@yahoo.com>
2018-08-02 20:34:41 +0000

    first commit

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - Entry point
+ * Description: print number of element in the linked list
+ * @head: first node
+ * @str: content of string
+ * Return: node.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *_strdup;
+
+	_strdup = strdup(str);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = _strdup;
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
