53775c233bf3ea5914f0fbc41adb2125586f950f
JackWanaCode <kazucuong@yahoo.com>
2018-08-03 05:40:09 +0000

    first commit

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void turtle_first(void) __attribute__ ((constructor));
+void turtle_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
