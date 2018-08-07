74d4a1089f9cddb1d841d15122e29ea63664883c
JackWanaCode <kazucuong@yahoo.com>
2018-08-07 15:55:37 +0000

    exit 98

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -7,14 +6,15 @@
  * print_listint_safe - Entry point
  * Description: print a listint_l linked list
  * @head: pointer to struct
- * Return: number of node.
+ * Return: number of node(s).
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *c = head;
 	long d = head - head->next;
 	int i = 0;
-
+	if (!c)
+		exit (98);
 	while (c != NULL)
 	{
 		printf("[%p] %i\n", (void *)c, c->n);
