38e34b4fdef8ddbe6da282a3f6d43cfc0a0478f7
JackWanaCode <kazucuong@yahoo.com>
2018-08-10 00:45:10 +0000

    correct case of max

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -11,27 +11,34 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int max = 1;
+	unsigned long int m = n;
 	int count = 0;
 
-	/* find maximum max that = 2^k but less than or equal n */
+	/* special case for unsigned long max, when max always less than n */
 	if (n == ULONG_MAX)
-		count = 32;
-	else
-		while (max << 1 <= n)
-		{
-			count++;
-			max <<= 1;
-		}
+	{
+ 		m = (m >> 1) + 1;
+		count++;
+	}
+
+	/*find maximum max that = 2^k but less than or equal  */
+	while (max << 1 <= m)
+	{
+		count++;
+		max = max << 1;
+		if (max == m)
+			break;
+ 	}
 
 	/* while loop to compare n and max */
 	while (count >= 0)
 	{
-		if (n >= max && n != 0)
+		if (m >= max && m != 0)
 		{
 			putchar('1');
-			n = n - max;
+			m = m - max;
 		}
-		else if (n < max || n == 0)
+		else if (m < max || m == 0)
 			putchar('0');
 		count--;
 		max >>= 1;
