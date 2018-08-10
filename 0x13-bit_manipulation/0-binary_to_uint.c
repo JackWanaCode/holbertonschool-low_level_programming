f0b0b2da727c61773c515c42d651c176f4e7444e
JackWanaCode <kazucuong@yahoo.com>
2018-08-10 01:14:38 +0000

    correct betty style

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Entry point
+ * Description: convert base 2 to base 10
+ * @b: string of binary num
+ * Return: unsigned base 10.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	unsigned int num2 = 1;
+	int i, j;
+
+	for (i = 0; b[i] != '\0' && b != NULL; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		if (j < i - 1)
+			num2 *= 2;
+		num += (b[j] - '0') * num2;
+	}
+	return (num);
+}
