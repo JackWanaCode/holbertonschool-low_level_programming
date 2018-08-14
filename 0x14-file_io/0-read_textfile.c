6eeda320e2c8fdacc667fe351f60623fe8dcda4a
JackWanaCode <kazucuong@yahoo.com>
2018-08-14 21:52:45 +0000

    malloc buffer

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -15,21 +15,37 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int fd, n = 0, bool = 1;
 	size_t i = 0;
-	char buff[1];
+	char *buff;
 
 	if (filename == NULL || letters == 0)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	while (read(fd, buff, 1) > 0 && i < letters)
+	while (i < letters && bool)
 	{
-		if (write(1, buff, 1) >= 0)
-			i++;
+		buff = malloc(sizeof(char) * 16);
+		if (buff == NULL)
+			return (0);
+		while (i < letters && n < 16 && bool)
+		{
+			if (read(fd, &buff[n], 1) > 0)
+			{
+				i++;
+				n++;
+			}
+			else
+			{
+				bool = 0;
+			}
+		}
+		if (write(1, buff, n) >= 0)
+			n = 0;
 		else
 			return (0);
+		free(buff);
 	}
 	close(fd);
 	return (i);
