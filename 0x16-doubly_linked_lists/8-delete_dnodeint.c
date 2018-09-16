e943406683a7e06ede1e910ee7796d1f7b8c6772
JackWanaCode <kazucuong@yahoo.com>
2018-09-16 17:57:03 +0000

    try with *head is NULL

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,11 +17,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *n = *head;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+/*	if (*head == NULL)
 	{
 		return (-1);
 	}
-	while (n || c)
+*/	while (n || c)
 	{
 		p = c;
 		c = n;
