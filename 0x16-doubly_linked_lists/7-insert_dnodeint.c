f3152f816cc5cbd5618cd80bc56c4271c8c53e92
JackWanaCode <kazucuong@yahoo.com>
2018-09-17 04:42:32 +0000

    refractor code line

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	while ((!(*h) && idx == 0) || p || c)
 	{
-		if (i == idx )
+		if (i == idx)
 		{
 			new_node->next = c;
 			new_node->prev = p;
