3acf979431dbca2a0932472969219617fa92aacb
JackWanaCode <kazucuong@yahoo.com>
2018-10-12 23:03:29 +0000

    correct printing the empty table

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -15,6 +15,7 @@ void hash_table_print(const hash_table_t *ht)
 
 	if (!ht)
 		return;
+	printf("{");
 	for (idx = 0; idx < ht->size; idx++)
 		if (ht->array[idx])
 		{
@@ -22,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (sign == 0)
 				{
-					printf("{'%s': '%s'", ht->array[idx]->key, ht->array[idx]->value);
+					printf("'%s': '%s'", ht->array[idx]->key, ht->array[idx]->value);
 					sign = 1;
 				}
 				else
