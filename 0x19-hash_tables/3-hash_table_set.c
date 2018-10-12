beebd94683d01d776a291e1564541cab2b09aaca
JackWanaCode <kazucuong@yahoo.com>
2018-10-12 22:14:47 +0000

    try with not check idx is within size

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,11 +14,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *hnode;
 
-	if (!ht || !key || strlen(key) == 0 || strlen(value) == 0)
+	if (!ht || !key || !*key || !value)
 		return (0);
 	idx = key_index((unsigned char *)key, 1024);
-	if (idx > ht->size)
-		return (0);
 	if (ht->array[idx])
 	{
 		for (hnode = ht->array[idx]; hnode; hnode = hnode->next)
