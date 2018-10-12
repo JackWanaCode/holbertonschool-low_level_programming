4e46de113a6b17575bb56fe0a7674aa225df3938
JackWanaCode <kazucuong@yahoo.com>
2018-10-12 22:52:58 +0000

    through the loop to find the key

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,13 +11,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int idx;
+	hash_node_t *curr;
 
 	if (!ht || !key || strlen(key) == 0)
 		return (NULL);
 	idx = key_index((unsigned char *)key, ht->size);
 	if (ht->array[idx])
 	{
-		return (ht->array[idx]->value);
+		for (curr = ht->array[idx]; curr; curr = curr->next)
+		{
+			if (strcmp(curr->key, key) == 0)
+				return (curr->value);
+		}
 	}
 	return (NULL);
 }
