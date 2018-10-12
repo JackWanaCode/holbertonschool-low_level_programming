49d7586e3e5838617e56ed64e55ec6ccdcac0d34
JackWanaCode <kazucuong@yahoo.com>
2018-10-12 19:39:16 +0000

    when size of hash table < key index

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,7 +14,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *hnode;
 
+	if (!ht || !key || strlen(key) == 0)
+		return (0);
 	idx = key_index((unsigned char *)key, 1024);
+	if (idx > ht->size)
+		return (0);
 	if (ht->array[idx])
 	{
 		for (hnode = ht->array[idx]; hnode; hnode = hnode->next)
@@ -28,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 	hnode = malloc(sizeof(hash_node_t));
-	if (!hnode || !ht || !key || strlen(key) == 0)
+	if (!hnode)
 		return (0);
 	hnode->key = strdup(key);
 	hnode->value = strdup(value);
