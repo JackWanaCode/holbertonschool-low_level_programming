3ce266ef5c98e9963ca569c18fac145402d19758
JackWanaCode <kazucuong@yahoo.com>
2018-10-12 20:30:43 +0000

    free ht before return NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -19,7 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	hashtb->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(hashtb->array))
+	{
+		free(hashtb);
 		return (NULL);
+	}
 	for (; i < size; i++)
 		hashtb->array[i] = NULL;
 	hashtb->size = size;
