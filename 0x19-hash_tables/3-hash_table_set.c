8cc739c7d1abef571681055801ede0af23cca42f
JackWanaCode <kazucuong@yahoo.com>
2018-10-12 21:47:10 +0000

    add check value is NULL

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *hnode;
 
-	if (!ht || !key || strlen(key) == 0)
+	if (!ht || !key || strlen(key) == 0 || strlen(value) == 0)
 		return (0);
 	idx = key_index((unsigned char *)key, 1024);
 	if (idx > ht->size)
