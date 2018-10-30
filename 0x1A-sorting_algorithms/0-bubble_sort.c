0de7d67b53435092befa3882f90033a1eb3fd0ef
JackWanaCode <kazucuong@yahoo.com>
2018-10-30 19:00:11 +0000

    fix print out

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -26,8 +26,9 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array, size);
 			}
-			print_array(array, size);
+
 		}
 		n++;
 	}
