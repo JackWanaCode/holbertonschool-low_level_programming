15466887664ae4741aac14aa9847679e8ca29571
Admin <Admin@Admins-Macintosh.local>
2018-10-30 15:59:12 -0700

    fix when j is 0 in insertion help function

--- 0x1A-sorting_algorithms/100-shell_sort.c
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -2,53 +2,77 @@
 #include <stdlib.h>
 #include "sort.h"
 
-void shell(int *array, size_t reach, size_t size);
+/**
+ * insertion_help - help to sort data by insertion
+ *
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
+ * @f_point: first point of array will be sorted
+ * @interval: interval will for a virtual sub array
+ * Return:
+ */
+
+void insertion_help(int *array, size_t size, size_t f_point, size_t interval)
+{
+	size_t i = f_point + interval;
+	size_t j;
+	int temp;
+
+	for (; i < size; i += interval)
+	{
+		temp = array[i];
+		j = i;
+		for (; temp < array[j - interval] && j > f_point; j -= interval)
+		{
+			array[j] = array[j - interval];
+
+		}
+		array[j] = temp;
+	}
+}
+
+/**
+ * shell_sort_help -  help function to sort data by shell_sort
+ *
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
+ * @interval: interval will for a virtual sub array
+ * Return:
+ */
+
+void shell_sort_help(int *array, size_t size, size_t interval)
+{
+	size_t i = 0;
+
+	for (; i < interval; i++)
+	{
+		insertion_help(array, size, i, interval);
+	}
+}
 
 
 /**
- * shell_sort - sorts array with shell sort
- * @array: array of integers
- * @size: size of array
+ * shell_sort - sort data by shell_sort
+ *
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
+ *
+ * Return:
  */
 
 void shell_sort(int *array, size_t size)
 {
-	size_t test = 0, reach;
-
-	while (test < size)
-		test = (test * 3) + 1;
-	reach = (test - 1) / 3;
-	shell(array, reach, size);
-}
-
-/**
- * shell - shell sort helper
- * @array: array
- * @reach: increment
- * @size: size of array
- */
-
-void shell(int *array, size_t reach, size_t size)
-{
-	size_t lo = 0, next = (reach - 1) / 3, inc = reach;
-	int temp;
+	size_t interval = 0;
 
 	if (!array || size < 2)
 		return;
-	while (reach < size)
+	for (; interval < size; interval = interval * 3 + 1)
+		;
+	interval = (interval - 1) / 3;
+	for (; interval > 0;)
 	{
-		if (array[lo] > array[reach])
-		{
-			temp = array[reach];
-			array[reach] = array[lo];
-			array[lo] = temp;
-			reach = inc;
-			lo = 0;
-		}
-		reach++;
-		lo++;
+		shell_sort_help(array, size, interval);
+		print_array(array, size);
+		interval = (interval - 1) / 3;
 	}
-	print_array(array, size);
-	if (next > 0)
-		shell(array, next, size);
 }
