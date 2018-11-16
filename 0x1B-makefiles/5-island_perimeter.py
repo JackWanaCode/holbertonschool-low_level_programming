a4bebb6e65c3b9373ed7c185ff2b9afe048a1b1d
JackWanaCode <kazucuong@yahoo.com>
2018-11-16 23:17:20 +0000

    first commit

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """return perimeter of island
+    """
+    count = 0
+    for row in range(1, len(grid) - 1):
+        for col in range(1, len(grid[0]) - 1):
+            if re_matrix[row][col] == 1:
+                if re_matrix[row - 1][col] == 0:
+                    count += 1
+                if re_matrix[row + 1][col] == 0:
+                    count += 1
+                if re_matrix[row][col + 1] == 0:
+                    count += 1
+                if re_matrix[row][col - 1] == 0:
+                    count += 1
+    return count
