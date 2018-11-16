68584ce1e601702cd180348ed414cfd15c1198f3
JackWanaCode <kazucuong@yahoo.com>
2018-11-16 23:19:12 +0000

    first commit

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -7,13 +7,13 @@ def island_perimeter(grid):
     count = 0
     for row in range(1, len(grid) - 1):
         for col in range(1, len(grid[0]) - 1):
-            if re_matrix[row][col] == 1:
-                if re_matrix[row - 1][col] == 0:
+            if grid[row][col] == 1:
+                if grid[row - 1][col] == 0:
                     count += 1
-                if re_matrix[row + 1][col] == 0:
+                if grid[row + 1][col] == 0:
                     count += 1
-                if re_matrix[row][col + 1] == 0:
+                if grid[row][col + 1] == 0:
                     count += 1
-                if re_matrix[row][col - 1] == 0:
+                if grid[row][col - 1] == 0:
                     count += 1
     return count
