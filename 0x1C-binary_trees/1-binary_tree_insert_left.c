7e94a39468bca3ec7e0b9fad9b359d441220ee86
JackWanaCode <kazucuong@yahoo.com>
2018-12-13 04:31:47 +0000

    return NULL when parent is NULL

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -13,17 +13,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 	binary_tree_t *left_node;
 
+	if (!parent)
+		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	if (!parent)
-	{
-		new_node->parent = NULL;
-		return (new_node);
-	}
 	left_node = parent->left;
 	parent->left = new_node;
 	new_node->parent = parent;
