88b99a77e476b1a74afef0f2340e5a6ba728c41a
JackWanaCode <kazucuong@yahoo.com>
2018-12-13 07:18:56 +0000

    fix case of full binary tree

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,20 +1,41 @@
 #include "binary_trees.h"
-
-#define CMP(x, y) ((x) == (y) ? (1) : (0))
-#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define CMP(x, y) ((x) == (y) ? 1 : 0)
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 /**
-* height - Entry point
-* Description - measures the height of a binary tree
+* binary_tree_depth - Entry point
+* Description - measures the depth of a binary tree
 * @tree: pointer point to root
-* Return: height
+* Return: nothing
 */
 
-size_t height(const binary_tree_t *tree)
+
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (!tree)
+	const binary_tree_t *jump;
+	size_t count = 0;
+
+	for (jump = tree; jump && jump->parent; jump = jump->parent)
+		count++;
+	return (count);
+}
+
+/**
+ * check_perfect - Entry point
+ * Description - measures the height of a binary tree
+ * @tree: pointer point to root
+ * @leaf_depth: depth of leaves
+ * Return: 1 if perfect, else 0
+ */
+
+int check_perfect(const binary_tree_t *tree, size_t leaf_depth)
+{
+	if (!tree->left && !tree->right)
+		return (CMP(binary_tree_depth(tree), leaf_depth));
+	else if (!tree->left || !tree->right)
 		return (0);
-	return (MAX(height(tree->left), height(tree->right)) + 1);
+	return (MIN(check_perfect(tree->left, leaf_depth),
+							check_perfect(tree->right, leaf_depth)));
 }
 
 /**
@@ -26,9 +47,13 @@ size_t height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t leaf_depth;
+	const binary_tree_t *jump;
+
 	if (!tree)
 		return (0);
-	if (CMP(height(tree->left), height(tree->right)) == 0)
-		return (0);
-	return (1);
+	for (jump = tree; jump->left; jump = jump->left)
+		;
+	leaf_depth = binary_tree_depth(jump);
+	return (check_perfect(tree, leaf_depth));
 }
