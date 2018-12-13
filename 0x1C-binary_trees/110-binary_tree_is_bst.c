f9975e201622ab0cb7fcb99194dfca0502e002cc
JackWanaCode <kazucuong@yahoo.com>
2018-12-13 10:01:14 +0000

    fix equal

--- 0x1C-binary_trees/110-binary_tree_is_bst.c
+++ 0x1C-binary_trees/110-binary_tree_is_bst.c
@@ -5,27 +5,27 @@
 * Description - check the tree is binary search tree?
 * @tree: pointer point to root
 * @key: key for comparison
+* @first: check for first time using
+* @check: will be return
 * Return: 1 if yes, else 0;
 */
 
-int check_bst(const binary_tree_t *tree, int *key, int *first)
+int check_bst(const binary_tree_t *tree, int *key, int *first, int *check)
 {
-	int check = 1;
 	if (tree->left)
-		check = check_bst(tree->left, key, first);
+		*check = check_bst(tree->left, key, first, check);
 	if (*first == 0)
 	{
 		*key = tree->n;
 		*first = 1;
 	}
 	else
-		if (*key > tree->n)
+		if (*key >= tree->n)
 			return (0);
-		else
-			*key = tree->n;
-	if (tree->right && check == 1)
-		check = check_bst(tree->right, key, first);
-	return (check);
+		*key = tree->n;
+	if (tree->right && *check == 1)
+		*check = check_bst(tree->right, key, first, check);
+	return (*check);
 }
 
 /**
@@ -38,8 +38,9 @@ int check_bst(const binary_tree_t *tree, int *key, int *first)
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	int key = 0;
-	int first = 0;
+	int first = 0, check = 1;
+
 	if (!tree)
 		return (0);
-	return (check_bst(tree, &key, &first));
+	return (check_bst(tree, &key, &first, &check));
 }
