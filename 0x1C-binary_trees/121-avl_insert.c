326f6a8e0b46534c29ae34f7bdf700eb3f56df14
JackWanaCode <kazucuong@yahoo.com>
2018-12-14 22:18:01 +0000

    correct loosing root

--- 0x1C-binary_trees/121-avl_insert.c
+++ 0x1C-binary_trees/121-avl_insert.c
@@ -26,7 +26,6 @@ void self_balance_help(avl_t *father, avl_t *current, avl_t *child)
 			(current->right && current->right->n == child->n))
 	{
 		binary_tree_rotate_left(father);
-
 	}
 	if ((father->right && father->right->n == current->n) &&
 			(current->left && current->left->n == child->n))
@@ -70,6 +69,43 @@ void set_self_balance(avl_t *node)
 }
 
 /**
+* avl_insert_f - Entry point
+* Description - help to donw level of double pointer
+* @tree: pointer point to root
+* @value: value of node
+* Return: added node pointer
+*/
+
+avl_t *avl_insert_f(avl_t *tree, int value)
+{
+	avl_t *temp;
+
+	if (value > tree->n)
+	{
+		if (!tree->right)
+		{
+			tree->right = binary_tree_node(tree, value);
+			temp = tree->right;
+			set_self_balance(tree->right);
+			return (temp);
+		}
+		return (avl_insert_f(tree->right, value));
+	}
+	else if (value < tree->n)
+	{
+		if (!tree->left)
+		{
+			tree->left = binary_tree_node(tree, value);
+			temp = tree->left;
+			set_self_balance(tree->left);
+			return (temp);
+		}
+		return (avl_insert_f(tree->left, value));
+	}
+	return (NULL);
+}
+
+/**
 * avl_insert - Entry point
 * Description - insert a node follow AVL tree
 * @tree: pointer point to root
@@ -79,33 +115,17 @@ void set_self_balance(avl_t *node)
 
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *temp;
+	avl_t *root, *node;
 
 	if (!(*tree))
 	{
-		*tree = binary_tree_node(NULL, value);
-		return (*tree);
+		root =  (binary_tree_node(NULL, value));
+		*tree = root;
+		return (root);
 	}
-	if (value > (*tree)->n)
-	{
-		if (!(*tree)->right)
-		{
-			(*tree)->right = binary_tree_node(*tree, value);
-			temp = (*tree)->right;
-			set_self_balance((*tree)->right);
-			return (temp);
-		}
-		return (avl_insert(&(*tree)->right, value));
-	}
-	else if (value < (*tree)->n)
-	{
-		if (!(*tree)->left)
-		{
-			(*tree)->left = binary_tree_node(*tree, value);
-			set_self_balance((*tree)->left);
-			return ((*tree)->left);
-		}
-		return (avl_insert(&(*tree)->left, value));
-	}
-	return (NULL);
+	root = *tree;
+	node = avl_insert_f(root, value);
+	for (; (*tree)->parent; (*tree) = (*tree)->parent)
+		;
+	return (node);
 }
