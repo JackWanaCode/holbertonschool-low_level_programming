2c8414bb5454a563448d08aa3e582817660cd24f
JackWanaCode <kazucuong@yahoo.com>
2018-12-15 19:52:25 +0000

    correct main.c

--- 0x1C-binary_trees/102-binary_tree_is_complete.c
+++ 0x1C-binary_trees/102-binary_tree_is_complete.c
@@ -1,21 +1,20 @@
 #include "binary_trees.h"
-#define CMP(x, y) ((x) == (y) ? 1 : 0)
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 /**
 * depth - Entry point
 * Description - measures the depth of a binary tree
-* @root: pointer point to root
+* @temp_root: pointer point to root
 * @node: pointer to node need to be get depth
 * Return: depth
 */
 
 
-size_t depth(binary_tree_t *root, binary_tree_t *node)
+size_t depth(binary_tree_t *temp_root, binary_tree_t *node)
 {
 	size_t count = 0;
 
-	for (; node != root; node = node->parent)
+	for (; node != temp_root; node = node->parent)
 		count++;
 	return (count);
 }
@@ -27,39 +26,63 @@ size_t depth(binary_tree_t *root, binary_tree_t *node)
 * @node: current node pointer
 * @leaf_depth: depth of leaf;
 * @count_left: left counting
+* @count_double: left counting
 * Return: 1 if yes, else 0
 */
 
 int complete_check(binary_tree_t *temp_root, binary_tree_t *node,
-										size_t leaf_depth, int *count_left)
+										size_t leaf_depth, int *count_left, int *count_double)
 {
+	int x = 0, y = 0;
+
 	if (!node)
 		return (1);
 	if (!node->left && node->right)
 		return (0);
-	if (depth(temp_root, node) > leaf_depth)
-		return (0);
+	if (depth(temp_root, node) == leaf_depth)
+	{
+		if (node->left || node->right)
+		{
+			return (0);
+		}
+		return (1);
+	}
 	if (depth(temp_root, node) == leaf_depth - 1)
 	{
 		if (node->left && !node->right)
 		{
-			if (*count_left == 0)
-				*count_left = 1;
+			if (*count_left < 1)
+			{
+				(*count_left)++;
+				return (1);
+			}
 			else
+			{
 				return (0);
+			}
 		}
 		if (!node->left && !node->right)
-			*count_left = 1;
-		if (node->left && node->right && *count_left == 1)
+		{
+			(*count_double)++;
+			return (1);
+		}
+		if (node->left && node->right && *count_double > 0)
+		{
 			return (0);
+		}
 	}
 	if (depth(temp_root, node) < leaf_depth - 1)
 	{
 		if (!node->left && !node->right)
+		{
 			return (0);
+		}
 	}
-	return (MIN(complete_check(temp_root, node->left, leaf_depth, count_left),
-							complete_check(temp_root, node->right, leaf_depth, count_left)));
+	x = complete_check(temp_root, node->left, leaf_depth,
+											count_left, count_double);
+	y = complete_check(temp_root, node->right, leaf_depth,
+											count_left, count_double);
+	return (MIN(x, y));
 }
 
 /**
@@ -74,6 +97,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	binary_tree_t *temp;
 	size_t leaf_depth = 0;
 	int count_left = 0;
+	int count_double = 0;
 
 	if (!tree)
 		return (0);
@@ -86,5 +110,5 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (leaf_depth == 0)
 		return (1);
 	return (complete_check((binary_tree_t *)tree, (binary_tree_t *)tree,
-													leaf_depth, &count_left));
+													leaf_depth, &count_left, &count_double));
 }
