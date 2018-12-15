601c67622f5d61694bf311e49135a76483c1e4fc
JackWanaCode <kazucuong@yahoo.com>
2018-12-15 07:08:33 +0000

    correct agian:

--- 0x1C-binary_trees/102-binary_tree_is_complete.c
+++ 0x1C-binary_trees/102-binary_tree_is_complete.c
@@ -1,118 +1,78 @@
 #include "binary_trees.h"
+#define CMP(x, y) ((x) == (y) ? 1 : 0)
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 /**
-* binary_tree_depth - Entry point
+* depth - Entry point
 * Description - measures the depth of a binary tree
-* @tree: pointer point to root
-* Return: nothing
+* @root: pointer point to root
+* @node: pointer to node need to be get depth
+* Return: depth
 */
 
 
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t depth(binary_tree_t *root, binary_tree_t *node)
 {
-	const binary_tree_t *jump;
 	size_t count = 0;
 
-	if (!tree)
-		return (0);
-	for (jump = tree; jump && jump->parent; jump = jump->parent)
+	for (; node != root; node = node->parent)
 		count++;
 	return (count);
 }
 
 /**
- * free_list - Entry point
- * Description: free list
- * @head: pointer to struct
- * Return: nothing
- */
-
-void free_list(blist_t *head)
-{
-	blist_t *jump;
-
-	for (jump = head; jump;)
-	{
-		head = head->next;
-		free(jump);
-		jump = head;
-	}
-}
-
-/**
 * complete_check - Entry point
-* Description - print the node as level order
-* @tree: pointer point to root
-* @leaf_depth: deapt of very left leaf
-* Return: height
+* Description - check tree is complete?
+* @temp_root: pointer point to root
+* @node: current node pointer
+* @leaf_depth: depth of leaf;
+* @count_left: left counting
+* Return: 1 if yes, else 0
 */
 
-int complete_check(binary_tree_t *tree, size_t leaf_depth)
+int complete_check(binary_tree_t *temp_root, binary_tree_t *node,
+										size_t leaf_depth, int *count_left)
 {
-	blist_t *jump;
-	blist_t *last;
-	blist_t *head;
-	size_t check = 0, node_depth = 0;
-
-	last = malloc(sizeof(blist_t));
-	if (!last)
+	if (depth(temp_root, node) > leaf_depth)
 		return (0);
-	last->node = tree;
-	last->next = NULL;
-	jump = last;
-	head = last;
-	while (jump)
+	else if (!node->left && !node->right)
 	{
-		node_depth = binary_tree_depth(jump->node);
-		if (check == 0 && (node_depth < leaf_depth) && !jump->node->right)
-			check = 1;
-		else if ((node_depth < leaf_depth) && check == 1 &&
-							(jump->node->right || jump->node->left))
+		if ((*count_left == 0 && (depth(temp_root, node) == leaf_depth - 1)))
 		{
-			free_list(head);
+			return (1);
+			*count_left = 1;
+		}
+		if (depth(temp_root, node) < leaf_depth - 1)
 			return (0);
-		}
-		else if (!jump->node->left && jump->node->right)
-		{
-			free_list(head);
-			return (0);
-		}
-		if (jump->node->left)
-		{
-			last->next = malloc(sizeof(blist_t));
-			if (!last->next)
-			{
-				free_list(head);
-				return (1);
-			}
-			last = last->next;
-			last->node = jump->node->left;
-			last->next = NULL;
-		}
-		if (jump->node->right)
-		{
-			last->next = malloc(sizeof(blist_t));
-			last = last->next;
-			last->node = jump->node->right;
-			last->next = NULL;
-		}
-		jump = jump->next;
+		return (1);
 	}
-	free_list(head);
-	return (1);
+	else if (!node->left && node->right)
+		return (0);
+	else if (node->left && !node->right)
+	{
+		if (*count_left == 0)
+		{
+			*count_left = 1;
+			return (1);
+		}
+		return (0);
+	}
+	return (MIN(complete_check(temp_root, node->left, leaf_depth, count_left),
+							complete_check(temp_root, node->right, leaf_depth, count_left)));
 }
 
 /**
 * binary_tree_is_complete - Entry point
-* Description - check tree complete
+* Description - check the binary tree is complete or not
 * @tree: pointer point to root
-* Return: nothing
+* Return: height
 */
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	size_t leaf_depth = 0;
 	binary_tree_t *temp;
+	size_t leaf_depth = 0;
+	int count_left = 0;
 
 	if (!tree)
 		return (0);
@@ -124,7 +84,6 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	}
 	if (leaf_depth == 0)
 		return (1);
-	if (!tree)
-		return (0);
-	return (complete_check((binary_tree_t *)tree, leaf_depth));
+	return (complete_check((binary_tree_t *)tree, (binary_tree_t *)tree,
+													leaf_depth, &count_left));
 }
