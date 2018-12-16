02413a40f0ac03ab978c2974f4bc4e1ed2a4502a
JackWanaCode <kazucuong@yahoo.com>
2018-12-16 05:46:21 +0000

    first commit

---
+++ 0x1C-binary_trees/132-array_to_heap.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+
+/**
+* array_to_heap - Entry point
+* Description - insert a node follow max heap from array
+* @array: pointer point to first element
+* @size: size of array
+* Return: root
+*/
+
+heap_t *array_to_heap(int *array, size_t size)
+{
+	heap_t *root;
+
+	if (size == 0)
+		return (NULL);
+	while (size-- > 0)
+		heap_insert(&root, *array++);
+	return (root);
+}
