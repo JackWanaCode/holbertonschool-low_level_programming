e8178cfbe25d85a18ebf93c35bfd7a6db903166f
JackWanaCode <kazucuong@yahoo.com>
2019-01-22 17:48:43 +0000

    first commit

---
+++ 0x1D-search_algorithms/100-jump.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * linear_s - Entry point
+ * Description: searches for a value in an array of integers
+ * using the Linear search algo
+ * @array: array of intergers
+ * @size: size of array
+ * @value: value for searching
+ * @position: position of index
+ * Return: postion of value or -1 if not found
+ */
+
+int linear_s(int *array, size_t size, int value, int position)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i + position, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
+
+/**
+ * jump_search - Entry point
+ * Description: searches for a value in a sorted array of integers
+ * using the Jump search algo
+ * @array: array of intergers
+ * @size: size of array
+ * @value: value for searching
+ * Return: position of value or -1 if not found
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t step = sqrt(size);
+	size_t i;
+	int check = 1;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < (size + step - 1) && check; i += step)
+	{
+		if (i >= size)
+		{
+			step = step - (i - size + 1);
+			i = size - 1;
+			check = 0;
+		}
+		if (array[i] >= value)
+		{
+			i -= step;
+			printf("Value found between indexes [%lu] and [%lu]\n",
+			       i, i + step);
+			return (linear_s(array + i, step + 1, value, i));
+		}
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	}
+	return (-1);
+}
