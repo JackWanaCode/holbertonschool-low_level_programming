fcead5e0e1bb187c95716acb14dec54cd3d4486c
JackWanaCode <kazucuong@yahoo.com>
2019-01-24 01:44:30 +0000

    corrent output format

--- 0x1D-search_algorithms/105-jump_list.c
+++ 0x1D-search_algorithms/105-jump_list.c
@@ -9,20 +9,23 @@
  * using the Linear search algo
  * @head: head of list
  * @size: size of array
+ * @step: step
  * @value: value for searching
  * @position: position of index
  * Return: postion of value or -1 if not found
  */
 
-listint_t *linear_s(listint_t *head, size_t size, int value, int position)
+listint_t *linear_s(listint_t *head, size_t size, size_t step,
+		    int value, int position)
 {
-	size_t i;
+	size_t i, j;
 	listint_t *run;
 
-	for (i = 0, run = head; i < size; i++, run = run->next)
+	for (i = position, j = 0, run = head; i < size && j < step;
+	     i++, run = run->next)
 	{
-		printf("Value checked array[%lu] = [%d]\n",
-		       i + position, run->n);
+		printf("Value checked at index [%lu] = [%d]\n",
+		       i, run->n);
 		if (run->n == value)
 			return (run);
 	}
@@ -41,29 +44,22 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	size_t step = sqrt(size), i = 0, k = 0;
 	listint_t *run, *prev;
-	int check = 1;
 
-	if (!list)
+	if (!list || size == 0)
 		return (NULL);
-	for (run = list, prev = list; i < (size + step - 1) && check; i += step)
+	for (run = list, prev = list; i < (size + step - 1); i += step)
 	{
-		if (i >= size)
-		{
-			step = step - (i - size + 1);
-			i = size - 1;
-			check = 0;
-		}
-		if (run->n >= value)
+		if (i >= size || run->n >= value)
 		{
 			i -= step;
 			printf("Value found between indexes [%lu] and [%lu]\n",
 			       i, i + step);
-			return (linear_s(prev, step + 1, value, i));
+			return (linear_s(prev, size, step + 1, value, i));
 		}
 		prev = run;
 		for (k = 0; k < step && run->next; k++)
 			run = run->next;
-		printf("Value checked array[%lu] = [%d]\n", i + step, run->n);
+		printf("Value checked at index [%lu] = [%d]\n", i + step, run->n);
 	}
 	return (NULL);
 }
